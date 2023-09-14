63acc71e7bd74d72b56a0deb64be64ff6b19be6f
Alexander <alexander.work.mailbox@gmail.com>
2023-09-14 17:31:54 +0600

    Added apollo provider

--- App.tsx
+++ App.tsx
@@ -1,28 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons'; 
 import Add from './src/components/Add';
+import Collections from './src/components/Collections';
+import Settings from "./src/components/Settings";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "http://192.168.0.100:4500/graphql",
+  cache: new InMemoryCache()
+});
 
 const Navigator = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigator.Navigator screenOptions={{
-        tabBarActiveTintColor: "black",
-        tabBarInactiveTintColor: "gray"
-      }}>
-        <Navigator.Screen name="Add" component={Add} options={{
-          tabBarIcon: ({ focused }) => <Ionicons name="language" size={24} color={focused ? "black" : "gray"} />
-        }}></Navigator.Screen>
-        <Navigator.Screen name="Collections" component={Add} options={{
-          tabBarIcon: ({ focused }) => <Ionicons name="copy" size={24} color={focused ? "black" : "gray"} />
-        }}></Navigator.Screen>
-        <Navigator.Screen name="Settings" component={Add} options={{
-          tabBarIcon: ({ focused }) => <Ionicons name="settings-sharp" size={24} color={focused ? "black" : "gray"}  />
-        }}></Navigator.Screen>
-      </Navigator.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Navigator.Navigator screenOptions={{
+          tabBarActiveTintColor: "black",
+          tabBarInactiveTintColor: "gray"
+        }}>
+          <Navigator.Screen name="Add" component={Add} options={{
+            tabBarIcon: ({ focused }) => <Ionicons name="language" size={24} color={focused ? "black" : "gray"} />
+          }}></Navigator.Screen>
+          <Navigator.Screen name="Collections" component={Collections} options={{
+            tabBarIcon: ({ focused }) => <Ionicons name="copy" size={24} color={focused ? "black" : "gray"} />
+          }}></Navigator.Screen>
+          <Navigator.Screen name="Settings" component={Settings} options={{
+            tabBarIcon: ({ focused }) => <Ionicons name="settings-sharp" size={24} color={focused ? "black" : "gray"}  />
+          }}></Navigator.Screen>
+        </Navigator.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
