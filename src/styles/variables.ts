526a50ba4ec2268f581c9a48117c712a68b93a3f
Alexander <alexander.work.mailbox@gmail.com>
2023-09-21 13:14:37 +0400

    Added style variables

---
+++ src/styles/variables.ts
@@ -0,0 +1,10 @@
+export const fontColor = "#222222";
+export const fontColorLight = "#eeeeee";
+export const fontColorFaint = "#555555a0";
+export const fontColorFaintLight = "#cccccccc";
+
+export const blue = "#4a6bff";
+export const green = "#41c948";
+export const yellow = "#e2d847";
+export const red = "#e74747";
+export const orange = "#e78747";
