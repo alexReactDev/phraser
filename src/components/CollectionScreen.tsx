68678ec812cd76ed3c6ce621cf9b239f9a70973d
Alexander <alexander.work.mailbox@gmail.com>
2023-09-21 13:13:54 +0400

    Added collection data loading

--- src/components/CollectionScreen.tsx
+++ src/components/CollectionScreen.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useQuery } from "@apollo/client";
 import { GET_COLLECTION_PHRASES } from "../query/phrases";
@@ -5,21 +6,25 @@ import Loader from "./Loader";
 import ErrorComponent from "./Error";
 import { IPhrase } from "../types/phrases";
 import CollectionPhrase from "./CollectionPhrase";
+import { GET_COLLECTION } from "../query/collections";
 
-function CollectionScreen({ route }: any) {
+function CollectionScreen({ route, navigation }: any) {
 	const colId = route.params.colId;
 
-	const { data = [], loading, error } = useQuery(GET_COLLECTION_PHRASES, { variables: { id: colId } })
+	const { data = [], loading, error } = useQuery(GET_COLLECTION_PHRASES, { variables: { id: colId } });
+	const { data: colData, loading: colDataLoading, error: colDataError } = useQuery(GET_COLLECTION, { variables: { id: colId } });
 
-	console.log(error);
+	useEffect(() => {
+		if(colData)  navigation.setOptions({ title: colData.getCollection.name });
+	}, [colData]);
 
-	if(loading) return <Loader />
-	if(error) return <ErrorComponent />
+	if(loading || colDataLoading) return <Loader />
+	if(error || colDataError) return <ErrorComponent />
 
 	return (
 		<View style={styles.container}>
 			{
-				data.getCollectionPhrases.map((phrase: IPhrase) => <CollectionPhrase phrase={phrase} />)
+				data.getCollectionPhrases.map((phrase: IPhrase) => <CollectionPhrase key={phrase.id} phrase={phrase} />)
 			}
 		</View>
 	)
--- src/query/collections.ts
+++ src/query/collections.ts
@@ -1,18 +1,37 @@
 import { gql } from "@apollo/client";
 
+export const GET_COLLECTION = gql`
+	query getCollection($id: ID) {
+		getCollection(id: $id) {
+			id,
+			name,
+			lastUpdate,
+			isLocked,
+			color,
+			created,
+			phrases,
+			repetitions,
+			meta {
+				phrasesCount,
+				repetitionsCount
+			}
+		}
+	}
+`;
+
 export const GET_COLLECTIONS_ALL = gql`
 	query GetCollections {
 		getCollections {
-			id
-			name
-			lastUpdate
-			isLocked
-			color
-			created
-			phrases
-			repetitions
+			id,
+			name,
+			lastUpdate,
+			isLocked,
+			color,
+			created,
+			phrases,
+			repetitions,
 			meta {
-				phrasesCount
+				phrasesCount,
 				repetitionsCount
 			}
 		}
