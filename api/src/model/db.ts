b5425aed8f78f578ae604375650dc90465e66f58
Alexander <alexander.work.mailbox@gmail.com>
2023-09-26 16:12:03 +0400

    Added mongo client

---
+++ api/src/model/db.ts
@@ -0,0 +1,28 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = "mongodb://127.0.0.1:27017/vocabulary";
+
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } catch(e) {
+	console.log("Database connection error");
+	console.log(e);
+  }
+}
+
+run();
+
+module.exports = client.db();
