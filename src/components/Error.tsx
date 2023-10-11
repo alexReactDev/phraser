60c7026d1b5709e0cdb75b0ad16e29a637ba92a7
Alexander <alexander.work.mailbox@gmail.com>
2023-10-11 12:30:05 +0400

    Added optional message to error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 
-function ErrorComponent() {
+function ErrorComponent({ message = "" }) {
 	return (
 		<View style={styles.errorContainer}>
 			<Text>
 				Error. Something went wrong
+			</Text>
+			<Text>
+				{message}
 			</Text>
 		</View>
 	)
