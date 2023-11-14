ea0de45baba866f33729be054320d123f083d6d7
Alexander <alexander.work.mailbox@gmail.com>
2023-11-14 22:31:37 +0400

    Changed profile id type in settings

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -8,12 +8,12 @@ export interface ISettings {
 	theme: string,
 	phrasesOrder: string,
 	repetitionsAmount: number,
-	activeProfile: number
+	activeProfile: string
 }
 
 export interface IInitialSettings {
 	theme: string | null,
 	phrasesOrder: string | null,
 	repetitionsAmount: number | null,
-	activeProfile: number | null
+	activeProfile: string | null
 }
