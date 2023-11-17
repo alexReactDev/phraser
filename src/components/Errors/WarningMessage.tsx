325bcca27461ffcf1359b08e6dd1a4ed16a94fe9
Alexander <alexander.work.mailbox@gmail.com>
2023-11-17 16:09:50 +0400

    Added warning message

---
+++ src/components/Errors/WarningMessage.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { fontColor, fontColorFaint } from "@styles/variables";
+
+function WarningMessage({ message = "" }) {
+	return (
+		<View style={styles.container}>
+			<View style={styles.titleContainer}>
+				<Ionicons name="information-circle-outline" size={21} color={fontColor} />
+				<Text style={styles.title}>
+					Something went wrong
+				</Text>
+			</View>
+			<Text style={styles.text}>
+				{message}
+			</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		borderWidth: 1,
+		borderStyle: "solid",
+		borderBlockColor: "gray",
+		borderRadius: 5,
+		padding: 10
+	},
+	titleContainer: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 8
+	},
+	title: {
+		fontSize: 18,
+		color: fontColor
+	}, 
+	text: {
+		textAlign: "center",
+		color: fontColorFaint
+	}
+})
+
+export default WarningMessage;
