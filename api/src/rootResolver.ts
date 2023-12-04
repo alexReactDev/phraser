712ef27e4bd8fdf4a60da856991f87b935ae3bd0
Alexander <alexander.work.mailbox@gmail.com>
2023-12-04 13:12:40 +0400

    Added wrapper functions to root resolver

--- api/src/rootResolver.ts
+++ api/src/rootResolver.ts
@@ -11,23 +11,23 @@ const autoCollectionsController = require("./controller/AutoCollections");
 const AIGeneratedTextController = require("./controller/AIGeneratedText");
 
 const root = {
-	login: authController.login,
-	signUp: authController.signUp,
-	logout: authController.logout,
-	getSession: authController.getSession,
-	getUser: usersController.getUser,
-	deleteUser: usersController.deleteUser,
+	login: (...args: any) => authController.login(...args),
+	signUp: (...args: any) => authController.signUp(...args),
+	logout: (...args: any) => authController.logout(...args),
+	getSession: (...args: any) => authController.getSession(...args),
+	getUser: (...args: any) => usersController.getUser(...args),
+	deleteUser: (...args: any) => usersController.deleteUser(...args),
 
-	getUserSettings: settingsController.getUserSettings,
-	setUserSettings: settingsController.setUserSettings,
-	updateUserSettings: settingsController.updateUserSettings,
+	getUserSettings: (...args: any) => settingsController.getUserSettings(...args),
+	setUserSettings: (...args: any) => settingsController.setUserSettings(...args),
+	updateUserSettings: (...args: any) => settingsController.updateUserSettings(...args),
 
-	createProfile: profilesController.createProfile,
-	getUserProfiles: profilesController.getUserProfiles,
-	mutateProfile: profilesController.mutateProfile,
-	deleteProfile: profilesController.deleteProfile,
+	createProfile: (...args: any) => profilesController.createProfile(...args),
+	getUserProfiles: (...args: any) => profilesController.getUserProfiles(...args),
+	mutateProfile: (...args: any) => profilesController.mutateProfile(...args),
+	deleteProfile: (...args: any) => profilesController.deleteProfile(...args),
 
-	createCollection: collectionsController.createCollection,
+	createCollection: (...args: any) => collectionsController.createCollection(...args),
 	generateAutoCollection: async ({ type }: { type: string }, context: { auth: IJWT}) => {
 		if(type === "auto") {
 			return await autoCollectionsController.createAutoCollection({}, context);
@@ -39,29 +39,29 @@ const root = {
 			throw new Error("400. Bad request");
 		}
 	},
-	getCollection: collectionsController.getCollection,
-	getProfileCollections: collectionsController.getCollectionsByProfile,
-	deleteCollection: collectionsController.deleteCollection,
-	mutateCollection: collectionsController.mutateCollection,
-	mutateCollectionMeta: collectionsController.mutateCollectionMeta,
-	changeCollectionLock: collectionsController.changeCollectionLock,
-	getPhraseCollection: collectionsController.getCollectionByPhrase,
+	getCollection: (...args: any) => collectionsController.getCollection(...args),
+	getProfileCollections: (...args: any) => collectionsController.getCollectionsByProfile(...args),
+	deleteCollection: (...args: any) => collectionsController.deleteCollection(...args),
+	mutateCollection: (...args: any) => collectionsController.mutateCollection(...args),
+	mutateCollectionMeta: (...args: any) => collectionsController.mutateCollectionMeta(...args),
+	changeCollectionLock: (...args: any) => collectionsController.changeCollectionLock(...args),
+	getPhraseCollection: (...args: any) => collectionsController.getCollectionByPhrase(...args),
 
-	getPhrase: phrasesController.getPhrase,
-	getCollectionPhrases: phrasesController.getPhrasesByCollection,
-	createPhrase: phrasesController.createPhrase,
-	mutatePhrase: phrasesController.mutatePhrase,
-	mutatePhraseMeta: phrasesController.mutatePhraseMeta,
+	getPhrase: (...args: any) => phrasesController.getPhrase(...args),
+	getCollectionPhrases: (...args: any) => phrasesController.getPhrasesByCollection(...args),
+	createPhrase: (...args: any) => phrasesController.createPhrase(...args),
+	mutatePhrase: (...args: any) => phrasesController.mutatePhrase(...args),
+	mutatePhraseMeta: (...args: any) => phrasesController.mutatePhraseMeta(...args),
 	movePhrase: (...args: any) => phrasesController.movePhrase(...args),
 	movePhrasesMany: (...args: any) => phrasesController.moveMany(...args),
 	deletePhrase: (...args: any) => phrasesController.deletePhrase(...args),
 	deletePhrasesMany: (...args: any) => phrasesController.deleteMany(...args),
 
-	createRepetition: repetitionsController.createRepetition,
-	getUserRepetitions: repetitionsController.getUserRepetitions,
+	createRepetition: (...args: any) => repetitionsController.createRepetition(...args),
+	getUserRepetitions: (...args: any) => repetitionsController.getUserRepetitions(...args),
 
 	getGeneratedText: (...args: any) => AIGeneratedTextController.generateText(...args),
-	getGeneratedSentences: AIGeneratedTextController.generateSentences
+	getGeneratedSentences: (...args: any) => AIGeneratedTextController.generateSentences(...args)
 }
 
 module.exports = root;
