96c7511dace927dcb55bd9e70f232ff6036b9196
Alexander <alexander.work.mailbox@gmail.com>
2023-12-05 15:32:15 +0400

    Added global error handling

--- App.tsx
+++ App.tsx
@@ -12,6 +12,10 @@ import { ClickOutsideProvider } from "react-native-click-outside";
 import { client } from "src/apollo";
 import ErrorMessageToast from "@components/Errors/ErrorMessageToast";
 import LoaderToast from "@components/Loaders/LoaderToast";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorComponent from "@components/Errors/ErrorComponent";
+import { setJSExceptionHandler } from "react-native-exception-handler";
+import { useEffect, useState } from "react";
 
 export type NavigatorParams = {
   Add: { mutateId: number | undefined } | undefined,
@@ -22,36 +26,48 @@ export type NavigatorParams = {
 const Navigator = createBottomTabNavigator<NavigatorParams>();
 
 export default function App() {
+  const [ error, setError ] = useState<any>(null);
+
+  useEffect(() => {
+    setJSExceptionHandler((error, isFatal) => {
+      if(isFatal) setError(error);
+    });
+  }, []);
+
+  if(error) return <ErrorComponent message="Fatal error. Try restarting the app" />
+
   return (
-    <ClickOutsideProvider>
-      <ApolloProvider client={client}>
-        <NavigationContainer>
-          <AuthorizationChecker>
-            <>
-              <ErrorMessageToast />
-              <LoaderToast />
-              <Navigator.Navigator 
-                id="MainNavigator" 
-                screenOptions={{
-                  tabBarActiveTintColor: "black",
-                  tabBarInactiveTintColor: "gray"
-                }}>
-                  <Navigator.Screen name="Add" component={Add} options={{
-                    tabBarIcon: ({ focused }) => <Ionicons name="language" size={24} color={focused ? "black" : "gray"} />,
-                    headerRight: () => <Profiles />
-                  }}></Navigator.Screen>
-                  <Navigator.Screen name="Collections" component={Collections} options={{
-                    tabBarIcon: ({ focused }) => <Ionicons name="copy" size={24} color={focused ? "black" : "gray"} />,
-                    headerShown: false
-                  }}></Navigator.Screen>
-                  <Navigator.Screen name="Settings" component={Settings} options={{
-                    tabBarIcon: ({ focused }) => <Ionicons name="settings-sharp" size={24} color={focused ? "black" : "gray"}  />
-                  }}></Navigator.Screen>
-              </Navigator.Navigator>
-            </>
-          </AuthorizationChecker>
-        </NavigationContainer>
-      </ApolloProvider>
-    </ClickOutsideProvider>
+    <ErrorBoundary fallback={<ErrorComponent message="Fatal error. Try restarting the app" />}>
+      <ClickOutsideProvider>
+        <ApolloProvider client={client}>
+          <NavigationContainer>
+            <AuthorizationChecker>
+              <>
+                <ErrorMessageToast />
+                <LoaderToast />
+                <Navigator.Navigator 
+                  id="MainNavigator" 
+                  screenOptions={{
+                    tabBarActiveTintColor: "black",
+                    tabBarInactiveTintColor: "gray"
+                  }}>
+                    <Navigator.Screen name="Add" component={Add} options={{
+                      tabBarIcon: ({ focused }) => <Ionicons name="language" size={24} color={focused ? "black" : "gray"} />,
+                      headerRight: () => <Profiles />
+                    }}></Navigator.Screen>
+                    <Navigator.Screen name="Collections" component={Collections} options={{
+                      tabBarIcon: ({ focused }) => <Ionicons name="copy" size={24} color={focused ? "black" : "gray"} />,
+                      headerShown: false
+                    }}></Navigator.Screen>
+                    <Navigator.Screen name="Settings" component={Settings} options={{
+                      tabBarIcon: ({ focused }) => <Ionicons name="settings-sharp" size={24} color={focused ? "black" : "gray"}  />
+                    }}></Navigator.Screen>
+                </Navigator.Navigator>
+              </>
+            </AuthorizationChecker>
+          </NavigationContainer>
+        </ApolloProvider>
+      </ClickOutsideProvider>
+    </ErrorBoundary>
   );
 }
