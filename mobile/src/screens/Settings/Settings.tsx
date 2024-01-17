e515a789c85b42baf995478bbb437e322cabe55e
Alexander <alexander.work.mailbox@gmail.com>
2024-01-17 15:31:27 +0400

    Added account management component

--- mobile/src/screens/Settings/Settings.tsx
+++ mobile/src/screens/Settings/Settings.tsx
@@ -1,72 +1,13 @@
-import { Alert, Button, ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView } from "react-native";
 import ProfilesSettings from "./components/ProfilesSettings";
 import LearnModeSettings from "./components/LearnModeSettings";
-import { useMutation } from "@apollo/client";
-import { DELETE_USER } from "../../query/user";
-import session from "../../store/session";
-import { removeAuthToken } from "../../utils/authToken";
-import { LOGOUT } from "../../query/authorization";
 import { observer } from "mobx-react-lite";
 import AutoCollectionsSettings from "./components/AutoCollectionsSettings";
 import AISettings from "./components/AISettings";
-import errorMessage from "@store/errorMessage";
 import SuggestionsSettings from "./components/SuggestionsSettings";
+import AccountManagement from "./components/AccountManagement";
 
 const Settings = observer(function() {
-	const [ deleteUser ] = useMutation(DELETE_USER);
-	const [ logout ] = useMutation(LOGOUT);
-
-	async function deleteUserHandler() {
-		Alert.alert("Are you sure?", "This action will delete your account permanently", [
-			{
-				text: "Cancel",
-				style: "cancel"
-			},
-			{
-				text: "Delete account",
-				style: "destructive",
-				onPress: async () => {
-					try {
-						await deleteUser({
-							variables: {
-								id: session.data.userId
-							},
-							context: {
-								headers: {
-									"Authorization": `Bearer ${session.data.token}`
-								}
-							}
-						})
-	
-						await removeAuthToken();
-						session.logout();
-					} catch (e: any) {
-						console.log(e);
-						errorMessage.setErrorMessage(e.toString());
-					}
-				}
-			}
-		])
-	}
-
-	async function logoutHandler() {
-		try {
-			await logout({
-				context: {
-					headers: {
-						"Authorization": `Bearer ${session.data.token}`
-					}
-				}
-			});
-
-			await removeAuthToken();
-			session.logout();
-		} catch (e: any) {
-			console.log(e);
-			errorMessage.setErrorMessage(e.toString());
-		}
-	}
-
 	return (
 		<ScrollView>
 			<ProfilesSettings />
@@ -74,27 +15,9 @@ const Settings = observer(function() {
 			<AutoCollectionsSettings />
 			<LearnModeSettings />
 			<AISettings />
-			<View style={styles.buttonsContainer}>
-				<View style={styles.button}>
-					<Button title="Logout" color="#ff6c6c" onPress={logoutHandler}/>
-				</View>
-				<View style={styles.button}>
-					<Button title="Delete account" color="red" onPress={deleteUserHandler}/>
-				</View>
-			</View>
+			<AccountManagement />
 		</ScrollView>
 	)
 });
-
-const styles = StyleSheet.create({
-	buttonsContainer: {
-		padding: 10,
-		flexDirection: "row",
-		justifyContent: "space-between"
-	},
-	button: {
-		width: "45%"
-	}
-})
 
 export default Settings;
---
+++ mobile/src/screens/Settings/components/AccountManagement.tsx
@@ -0,0 +1,106 @@
+import { removeAuthToken } from "@utils/authToken";
+import SettingsGroup from "./SettingsGroup";
+import session from "@store/session";
+import errorMessage from "@store/errorMessage";
+import { useMutation } from "@apollo/client";
+import { LOGOUT } from "@query/authorization";
+import { DELETE_USER } from "@query/user";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { observer } from "mobx-react-lite";
+
+const AccountManagement = observer(function () {
+	const [ deleteUser ] = useMutation(DELETE_USER);
+	const [ logout ] = useMutation(LOGOUT);
+
+	async function deleteUserHandler() {
+		Alert.alert("Are you sure?", "This action will delete your account permanently", [
+			{
+				text: "Cancel",
+				style: "cancel"
+			},
+			{
+				text: "Delete account",
+				style: "destructive",
+				onPress: async () => {
+					try {
+						await deleteUser({
+							variables: {
+								id: session.data.userId
+							},
+							context: {
+								headers: {
+									"Authorization": `Bearer ${session.data.token}`
+								}
+							}
+						})
+	
+						await removeAuthToken();
+						session.logout();
+					} catch (e: any) {
+						console.log(e);
+						errorMessage.setErrorMessage(e.toString());
+					}
+				}
+			}
+		])
+	}
+
+	async function logoutHandler() {
+		try {
+			await logout({
+				context: {
+					headers: {
+						"Authorization": `Bearer ${session.data.token}`
+					}
+				}
+			});
+
+			await removeAuthToken();
+			session.logout();
+		} catch (e: any) {
+			console.log(e);
+			errorMessage.setErrorMessage(e.toString());
+		}
+	}
+
+	return (
+		<SettingsGroup title="Account management">
+			<View style={styles.container}>
+				<TouchableOpacity style={styles.option}
+					activeOpacity={0.5}
+					onPress={logoutHandler}
+				>
+					<Text style={{...styles.optionText, color: "#9c2323"}}>
+						Log out
+					</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.option}
+					activeOpacity={0.5}
+					onPress={deleteUserHandler}
+				>
+					<Text style={{...styles.optionText, color: "#9c2323"}}>
+						Delete account
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</SettingsGroup>
+	)
+});
+
+const styles = StyleSheet.create({
+	container: {
+		gap: -1
+	},
+	option: {
+		borderWidth: 1,
+		borderColor: "#eee",
+		borderStyle: "solid",
+		borderRadius: 2,
+		padding: 8
+	},
+	optionText: {
+		fontSize: 15
+	}
+})
+
+export default AccountManagement;
