bf475bb68a988ffedd25bdb595803db5ec9e8199
Alexander <alexander.work.mailbox@gmail.com>
2024-04-10 14:13:39 +0400

    Added worker for sendPhraseOfTheDay notifications service

--- api/src/controller/Notifications.ts
+++ api/src/controller/Notifications.ts
@@ -1,6 +1,5 @@
 import globalErrorHandler from "../misc/globalErrorHandler";
 import db from "../model/db";
-import phrasesController from "./Phrases";
 
 class NotificationsController {
 	async updateNotificationsToken({ userId, token }: { userId: string, token: string }) {
@@ -39,97 +38,6 @@ class NotificationsController {
 		}
 
 		return "OK";
-	}
-
-	async sendPhraseOfTheDayNotifications() {
-		let eligibleUsers;
-
-		try {
-			const cursor = await db.collection("settings").find({ "settings.phraseOfTheDayReminderEnabled": {
-				$eq: true
-			}});
-
-			eligibleUsers = await cursor.toArray();
-		}
-		catch(e: any) {
-			console.log(e)
-			throw new Error(`Server error. Failed to get eligible users settings ${e}`)
-		}
-
-		const results = [];
-		let successfulSends = 0
-
-		for (let user of eligibleUsers) {
-			let tokens;
-
-			try {
-				const data = (await db.collection("users_tokens").findOne({ userId: user.userId }));
-				tokens = data?.expoPushTokens || [];
-			}
-			catch(e: any) {
-				globalErrorHandler(e);
-				results.push(new Error(`Server error. Failed to get user notifications tokens \n ${e} \n ${user.userId}`));
-			}
-
-			let phraseOfTheDay;
-
-			try {
-				phraseOfTheDay = await phrasesController.getPhraseOfTheDay({ userId: user.userId });
-			} catch (e: any) {
-				globalErrorHandler(e);
-				results.push(new Error(`Server error. Failed to get phrase of the day for user \n ${e} \n ${user.userId}`))
-			}
-
-			for (let token of tokens) {
-				try {
-					const res = await fetch('https://exp.host/--/api/v2/push/send', {
-						method: 'POST',
-						headers: {
-						  Accept: 'application/json',
-						  'Accept-encoding': 'gzip, deflate',
-						  'Content-Type': 'application/json',
-						},
-						body: JSON.stringify({
-							data: {
-								phrase: phraseOfTheDay.id,
-								value: phraseOfTheDay.value,
-								translation: phraseOfTheDay.translation
-							},
-							to: token
-						}),
-					});
-					
-					if(!res.ok) throw res;
-					const ticket = await res.json();
-
-					if(ticket.data.status != "ok") throw ticket;
-
-					results.push(ticket);
-					successfulSends++;
-				} catch (e: any) {
-					globalErrorHandler(e);
-
-					if(e.message?.includes("is not a registered push notification recipient")) {
-						await db.collection("users_tokens").updateOne({
-							expoPushTokens: {
-								$pull: token
-							}
-						})
-					}
-
-					results.push(new Error(`Server error. Failed to send notification to user \n ${e} \n ${user.userId}`))
-				}
-			}
-		}
-
-		if(successfulSends === 0 && eligibleUsers.length !== 0) {
-			const error = new Error("Failed to send notifications to users");
-			//@ts-ignore custom field
-			error.tickets = results;
-			throw error;
-		}
-
-		return results;
 	}
 }
 
--- api/src/services/sendPhraseOfTheDayNotificationsService.ts
+++ api/src/services/sendPhraseOfTheDayNotificationsService.ts
@@ -1,12 +1,14 @@
-import notificationsController from "../controller/Notifications";
+import { Worker } from "worker_threads";
 import globalErrorHandler from "../misc/globalErrorHandler";
 
 let retries = 0;
 
 export default async function sendPhraseOfTheDayNotificationService() {
-	try {
-		await notificationsController.sendPhraseOfTheDayNotifications();
-	} catch (e: any) {
+	const worker = new Worker("./src/services/sendPhraseOfTheDayNotificationWorker.ts", {
+		execArgv: ["--require", "ts-node/register"]
+	});
+
+	function errorHandler(e: any) {
 		if(retries >= 2) {
 			globalErrorHandler(new Error(`Failed to send phrase of the day notifications after ${retries} retries. \n ${e}`));
 			retries = 0;
@@ -16,4 +18,20 @@ export default async function sendPhraseOfTheDayNotificationService() {
 		setTimeout(sendPhraseOfTheDayNotificationService, retries === 0 ? 1000 * 2 : 1000 * 5);
 		retries++;
 	}
+
+	worker.on("error", (e) => {
+		errorHandler(e);
+	});
+
+	worker.on("exit", (code) => {
+		if(code !== 0) {
+			errorHandler(`Process exited with status code ${code}`);
+		}
+	});
+
+	worker.on("message", (message) => {
+		if(!message.done) {
+			errorHandler("No notifications were successfully sent");
+		}
+	});
 }
